1ad2ed7300646248286aab7b22c31adc2b1e4597
s1owjke <s1ow933@yahoo.com>
2020-12-14 21:53:48 +0300

    Add basic tests

--- tests/context.test.js
+++ tests/context.test.js
@@ -1,6 +1,67 @@
-import { expect } from "@open-wc/testing";
+import { LitElement, html } from "lit-element";
+import { expect, fixture, elementUpdated } from "@open-wc/testing";
 import { ProviderMixin, ConsumerMixin } from "../src/context.js";
 
-describe("context", () => {
-    // todo: value changing on dom
+class TestProvider extends ProviderMixin(LitElement) {
+    constructor() {
+        super();
+        this.value = 1;
+    }
+    static get properties() {
+        return {
+            value: Number,
+        };
+    }
+    static get provide() {
+        return ["value"];
+    }
+    render() {
+        return html` <test-consumer></test-consumer> `;
+    }
+}
+
+class TestConsumer extends ConsumerMixin(LitElement) {
+    constructor() {
+        super();
+        this.value = null;
+    }
+    static get properties() {
+        return {
+            value: Number,
+        };
+    }
+    static get inject() {
+        return ["value"];
+    }
+    render() {
+        return html` <div>${JSON.stringify(this.value)}</div> `;
+    }
+}
+
+customElements.define("test-provider", TestProvider);
+customElements.define("test-consumer", TestConsumer);
+
+describe("lit-elemt", () => {
+    it("to have default value if no provider exists", async () => {
+        const consumerEl = await fixture(html`<test-consumer></test-consumer>`);
+
+        expect(consumerEl.value).to.equal(null);
+    });
+
+    it("inject props value on after connect", async () => {
+        const providerEl = await fixture(html`<test-provider></test-provider>`);
+        const consumerEl = providerEl.shadowRoot.querySelector("test-consumer");
+
+        expect(consumerEl).shadowDom.equal("<div>1</div>");
+    });
+
+    it("update props values then context changes", async () => {
+        const providerEl = await fixture(html`<test-provider></test-provider>`);
+        const consumerEl = providerEl.shadowRoot.querySelector("test-consumer");
+
+        providerEl.value = 2;
+        await elementUpdated(consumerEl);
+
+        expect(consumerEl).shadowDom.equal("<div>2</div>");
+    });
 });
